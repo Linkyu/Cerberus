5dd7b1ee62b9ce7a41d842c33794c0a223671e88
Linkyu <linkyu@users.noreply.github.com>
2018-03-05 19:56:43 +0100

    Complete mono-thread prototype for Module 1

--- Analyzer/Program.cs
+++ Analyzer/Program.cs
@@ -12,20 +12,27 @@ namespace Analyzer
         {
             var stopWatch = new Stopwatch();
             stopWatch.Start();
-            
             using(var reader = new StreamReader(@"..\..\resources\genome-manusporny.txt"))
             {
-                var dict = new Dictionary<char, int>();
+                const char separator = '\t';
+                var genotypesDictionary = new Dictionary<char, int>();
                 var i = 0;
                 var unknowns = 0;
                 var dashes = 0;
+                
+                
+                reader.ReadLine();  // Skip the first line (header)
                 
                 while (!reader.EndOfStream)
                 {
                     var line = reader.ReadLine();
                     if (line == null) continue;
                     
-                    var values = line.Split('\t');
+                    var values = line.Split(separator);
+
+                    //int.TryParse(values[2], out var pos);
+                    //orderedGenome.Insert(SearchIndex(orderedGenome, pos), line);
+                    //var index = orderedGenome.BinarySearch(pos);
 
                     var key = values[3].ToUpper();
                     if (key.Length == 2)
@@ -34,13 +41,13 @@ namespace Analyzer
                         {
                             if (!new List<char> {'A', 'T', 'C', 'G'}.Contains(c)) continue;
                             
-                            if (dict.ContainsKey(c))
+                            if (genotypesDictionary.ContainsKey(c))
                             {
-                                dict[c]++;
+                                genotypesDictionary[c]++;
                             }
                             else
                             {
-                                dict[c] = 1;
+                                genotypesDictionary[c] = 1;
                             }
                         }
 
@@ -55,18 +62,49 @@ namespace Analyzer
                     }
 
                     i++;
+                    //Console.WriteLine(i);
                 }
 
                 Console.WriteLine();
                 Console.WriteLine(i + " pairs read");
 
-                foreach (var pair in dict)
+                foreach (var pair in genotypesDictionary)
                 {
                     Console.WriteLine(pair.Key + ": " + pair.Value + " - " + (float)pair.Value / i * 50 + "%");
                 }
 
-                Console.WriteLine("--: " + unknowns + " - " + (float)dashes / i * 50 + "%");
+                Console.WriteLine("--: " + dashes + " - " + (float)dashes / i * 50 + "%");
                 Console.WriteLine("??: " + unknowns + " - " + (float)unknowns / i * 50 + "%");
+
+                var lines = File.ReadAllLines(@"..\..\resources\genome-manusporny.txt").ToList();
+                lines.RemoveAt(0);
+                var orderedLines = lines.OrderBy(s => int.Parse(s.Split(separator)[2])).ToList();
+                var sequencesDictionary = new Dictionary<string, int>();
+                
+                for (var j = 0; j < orderedLines.Count - 1; j++)
+                {
+                    var key = orderedLines[j].Split(separator)[3] + orderedLines[j+1].Split(separator)[3];
+                    if (key.Length != 4 || key.Contains('D') || key.Contains('I') || key.Contains('-')) continue;
+                    
+                    if (sequencesDictionary.ContainsKey(key))
+                    {
+                        sequencesDictionary[key]++;
+                    }
+                    else
+                    {
+                        sequencesDictionary[key] = 1;
+                    }
+                }
+                
+                var sortedSequencesDictionary = from entry in sequencesDictionary orderby entry.Value descending select entry;
+
+                Console.WriteLine();
+                Console.WriteLine("4-sequences occurences:");
+                foreach (var pair in sortedSequencesDictionary)
+                {
+                    Console.WriteLine(pair.Key + ": " + pair.Value);
+                    //break; // We only want the highest one
+                }
             }
             
             stopWatch.Stop();
