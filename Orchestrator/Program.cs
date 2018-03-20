7ca464400cfd61feb4e3fcf5b92b08ce2d9fc3ec
daynox <doureyd@gmail.com>
2018-03-20 18:51:43 +0100

    Solve #5

--- Orchestrator/Program.cs
+++ Orchestrator/Program.cs
@@ -6,72 +6,93 @@ using System.Threading;
 
 namespace Orchestrator
 {
-  internal static class Orchestartor
-  {
-    private static List<TcpClient> _listConnectedClients;
-    private static int ClientNbr { get; set; } = 0;
-
-    private static void ThreadProc(object clientData)
+    internal static class Orchestartor
     {
-      var client = (TcpClient) clientData;//.clientRef;
-      Console.WriteLine("Log: A new client has connected");// " + clientData.nbr);
-      while(true)
-      {
-        if(client.Connected)
+        private static List<Client> _connectedClients;
+        private static int ClientId { get; set; } = 0;
+
+        private static void ThreadProc(object clientData)
         {
-          if(client.Client.Poll(10,SelectMode.SelectRead) && client.Available==0)
-          {
-            Console.WriteLine("Log: A client has disconnected");// " + clientData.nbr);
-            Thread.CurrentThread.Abort();
-          }
+            var clientObject = (Client) clientData;
+            Console.WriteLine($"Log: The client with id {clientObject.Id} has connected");
+            while (true)
+            {
+                if (!clientObject.IsConnected())
+                {
+                    Console.WriteLine($"Log: Client with id {clientObject.Id} has disconnected");
+                    _connectedClients.Remove(clientObject);
+                    Thread.CurrentThread.Abort();
+                }
+                Thread.Sleep(5000);
+            }
         }
-        Thread.Sleep(5000);
-      }
-    }
-    private static void StartListening(){
-      TcpListener listener=null;
-      _listConnectedClients = new List<TcpClient>();
-      try
-      {
-        // Set the listened port to 13000
-        const int port = 13000;
-        // TcpListener server = new TcpListener(port);
-        listener = new TcpListener(IPAddress.Any, port);
-        // Start listening for client requests.
-        listener.Start();
-        // Create a thread for all new client
-        while (true)
+
+        private static void StartListening()
         {
-          ClientNbr++;
-          var clientData = new { nbr = ClientNbr, clientRef = listener.AcceptTcpClient() }; 
-          _listConnectedClients.Add(clientData.clientRef);
-          ThreadPool.QueueUserWorkItem(ThreadProc, clientData.clientRef);
+            TcpListener listener = null;
+            _connectedClients = new List<Client>();
+            try
+            {
+                // Set the listened port to 13000
+                const int port = 13000;
+                // TcpListener server = new TcpListener(port);
+                listener = new TcpListener(IPAddress.Any, port);
+                // Start listening for client requests.
+                listener.Start();
+                // Create a thread for all new client
+                while (true)
+                {
+                    ClientId++;
+                    var client = new Client(ClientId, listener.AcceptTcpClient());
+                    _connectedClients.Add(client);
+                    ThreadPool.QueueUserWorkItem(ThreadProc, client);
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {
+                // Stop listening for new clients.
+                listener?.Stop();
+            }
         }
-      }
-      catch(SocketException e)
-      {
-        Console.WriteLine("SocketException: {0}", e);
-      }
-      finally
-      {
-        // Stop listening for new clients.
-        listener?.Stop();
-      }
+
+        public static void Main()
+        {
+            var socketThread = new Thread(StartListening);
+            socketThread.Start();
+            Console.WriteLine("\nHit enter to refresh the clients");
+            while (true)
+            {
+                Console.Read();
+                Console.Clear();
+                foreach (var client in _connectedClients)
+                {
+                    Console.WriteLine(client.Reference.Client.RemoteEndPoint);
+                }
+            }
+        }
     }
 
-    public static void Main()
+    public class Client
     {
-      var socketThread = new Thread (Orchestartor.StartListening);
-      socketThread.Start();
-      Console.WriteLine("\nHit enter to refresh the clients");
-      while (true)
-      {
-        Console.Read();
-        Console.Clear();
-        foreach (var client in _listConnectedClients) {
-          Console.WriteLine(client.Client.LocalEndPoint.ToString() + " " + client.Connected + "1");
+        public int Id { get; }
+        public TcpClient Reference { get; }
+
+
+        public Client(int id, TcpClient reference)
+        {
+            this.Id = id;
+            this.Reference = reference;
         }
-      }
+
+        public bool IsConnected()
+        {
+            if (!Reference.Connected) return true;
+            if (!Reference.Client.Poll(10, SelectMode.SelectRead) || Reference.Available != 0) return true;
+            return false;
+        }
     }
-  }
 }
