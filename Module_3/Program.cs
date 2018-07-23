d63debee21d2eed3dc8297132335704dd620fd1e
Jean Eric Nzaramba <jeaneric.nzaramba@gmail.com>
2018-07-23 11:00:21 +0200

    WIP : Recherche d'un gene dans le genome. v4

--- Module_3/Program.cs
+++ Module_3/Program.cs
@@ -9,43 +9,47 @@ namespace Module_3
     {
         public static void Main()
         {
-            
-            var numChromosome = 2; //pour l'instant, on ne focalise la recherche que sur un chromosome;
+            var numChromosome = 20; //pour l'instant, on ne focalise la recherche que sur un chromosome;
             const string genomeFile = @"..\..\elgrian.txt";
-            List<string>occurences = new List<string>();  // la liste des bases azotées pour un chromosome
-            using (var reader = new StreamReader(genomeFile)) {
-
-                reader.ReadLine();
-                //TODO
-                while (!reader.EndOfStream)
+            List <string>occurences = new List<string>();  // la liste des bases azotées pour un chromosome
+            try
+            {
+                using (var reader = new StreamReader(genomeFile))
                 {
-                    var fileLine = reader.ReadLine();
-                    if (!fileLine.StartsWith("#")) {
-
-
-                    var tokens = fileLine.Split('\t');
-
-                    int chromosome = -1;
-
-                    if (int.TryParse(tokens[1], out chromosome) && chromosome == numChromosome)
+                    reader.ReadLine();
+                    while (!reader.EndOfStream)
                     {
+                        var fileLine = reader.ReadLine();
+                        if (!fileLine.StartsWith("#"))
+                        {
 
-                        occurences.Add(fileLine); //toutes les occurences du fichier pour un chromosome donné
+                            var tokens = fileLine.Split('\t');
+
+                            var chromosome = -1;
+
+                            if (int.TryParse(tokens[1], out chromosome) && chromosome == numChromosome)
+                            {
+                                occurences.Add(fileLine); //toutes les occurences du fichier pour un chromosome donné
+                            }
+                        }
                     }
                 }
             }
-            }
+            catch (FileNotFoundException err) {
+                 Console.WriteLine(genomeFile + " introuvable.");
+            } 
+
             //Rechercher un gène dans un chromosome donné
             var basesList = new List<string>();
             StringBuilder builder = new StringBuilder();
             foreach (var occurence in occurences) {
                 builder = new StringBuilder();
                 var tokens = occurence.Split('\t');
-                char firstBase = tokens[3][0];
-                if (firstBase == '-' || firstBase == ' ' || firstBase == 'I' || firstBase == 'D' || firstBase == 'T') {
+                var  firstBase = tokens[3][0];
+                if (firstBase == '-' || firstBase == ' ' || firstBase == 'I' || firstBase == 'D') {
                 } else {
-                  //ajouter les couples à la liste des bases
-                  basesList.Add(tokens[3]);
+                    //ajouter les couples à la liste des bases
+                    basesList.Add(tokens[3]);
                 }
             }
 
@@ -53,32 +57,53 @@ namespace Module_3
             {
                 builder.Append(baseCouple[0]);
             }
-
-            //int nbCodonsMax = 129;
             
             //Premiere phase : à partir de la première base azotée.
-            string firstDnaStrand = builder.ToString();
+            var firstDnaStrand = builder.ToString();
             
             
-            //builder = new StringBuilder();
-
-            /*for (var position = 0; position < 3 * nbCodonsMax; position++)
-            {
-                builder.Append(FirstDnaStrand[position]);
-            }*/
-
-            //FirstDnaStrand = builder.ToString();
-            Console.WriteLine(firstDnaStrand);
             //Chercher le premier codon stop*
             var firstStop = firstDnaStrand.IndexOf("TAA", new StringComparison());
-            if (firstStop == -1) {
-                Console.WriteLine("Aucun codon STOP trouvé");
-            }
-
-            byte[] str = new byte[15];
-            builder = new StringBuilder(49511);
+            Console.WriteLine(firstStop == -1 ? "Aucun codon STOP trouvé" : "Codon stop trouvé à la ligne : " + firstStop);
             Console.WriteLine(firstDnaStrand.Length + " nucleotides presents dans la sequence sur " + builder.Capacity + " nucleotides possibles.");
             
+            //Chercher le deuxieme codon stop
+            var secondStop = firstDnaStrand.IndexOf("TAA", firstStop + 3, new StringComparison());
+            Console.WriteLine(secondStop == -1 ? "Aucun codon STOP trouvé" : "Codon stop trouvé à la ligne : " + secondStop);
+            var initiationIndex = -1;
+                
+            if (secondStop - firstStop - 3 > 0)
+            {
+                //Chercher un codon start
+                initiationIndex = firstDnaStrand.IndexOf("ATG", firstStop + 3, new StringComparison());
+                if (initiationIndex > -1)
+                {
+                    Console.WriteLine("Un codon start a été trouvé à l'index " + initiationIndex);
+                }
+            }
+
+            if (initiationIndex < secondStop && initiationIndex > firstStop)
+            {
+                //le start est situé entre deux codon stop
+                Console.WriteLine(
+                    initiationIndex > -1 && ((secondStop + 3) - initiationIndex) > 0 &&
+                    ((secondStop + 3) - initiationIndex) % 3 == 0
+                        ? "Une séquence codante a peut être été trouvé."
+                        : (secondStop + 3) - initiationIndex +
+                          " est négatif ou n'est pas un multiple de 3. La séquence est non codante.");
+            }
+
+            builder = new StringBuilder();
+            //Affichage de la séquence codante.
+            if (((secondStop + 3) - initiationIndex) % 3 == 0)
+            {
+                for (var i = initiationIndex; i < secondStop + 3; i++)
+                {
+                    builder.Append(firstDnaStrand[i]);
+                }
+
+                Console.WriteLine(builder.ToString());
+            }
         }
     }
 }
